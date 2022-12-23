3ded005e93ba36bf4bab647c9b130ec539c4ee74
Tamu <5020311+Tamu@users.noreply.github.com>
2022-12-23 15:59:09 +0100

    Update build tools

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,30 @@
 
-FROM nginx:alpine
+# Stage 0, Build and compile the frontend (based on Node.js)
+FROM node:16.4.0-alpine as build-stage
 
-RUN apk upgrade --update \
-	&& apk add vim
+WORKDIR /app
+
+# Build in relation to the environment
+# build / build:dev / build:stage
+ARG BUILDCMD=build
+
+COPY package*.json /app/
+
+RUN npm install
+
+COPY ./ /app/
+
+RUN echo "Build environment : $BUILDCMD"
+RUN npm run $BUILDCMD
+
+# Stage 1, Compiled app (based on Nginx)
+FROM nginx:stable-alpine
 
 RUN rm /usr/share/nginx/html/50x.html 
 RUN rm /usr/share/nginx/html/index.html
 
-COPY dist /usr/share/nginx/html
-COPY nginx-default.conf /etc/nginx/conf.d/default.conf
-COPY ./docker-host.sh /
-RUN ["chmod", "+x", "/docker-host.sh"]
+COPY --from=build-stage /app/dist/ /usr/share/nginx/html
+
+# Copy the nginx configuration from source
+COPY ./nginx-default.conf /etc/nginx/conf.d/default.conf
+COPY ./docker-host.sh ./docker-entrypoint.d/01-config-api-host.sh
--- docker-host.sh
+++ docker-host.sh
@@ -1,10 +1,39 @@
-#!/bin/bash -e
-NEWHOST='"host":"'$HOST'"'
+#!/bin/sh
 
+set -e
+
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+ME=$(basename $0)
+
+NEWHOST='"host":"'$HOST'"'
 LOCALHOST='"host":"http://localhost:3000"'
+LOCALHOST1='"host":"https://api.swissdata.io/prod"'
+
+NEWTMAPIKEY='"translationMemoryApiKey":"'$TMAPIKEY'"'
+LOCALAPIKEY='"translationMemoryApiKey":"ca5e3b78367c"'
+
+NEWTMHOST='"translationMemoryHost":"'$TMHOST'"'
+LOCALTMHOST='"translationMemoryHost":"http://localhost:3000"'
+LOCALTMHOST1='"translationMemoryHost":"https://api.swissdata.io/prod"'
 
 
 for jsfile in /usr/share/nginx/html/*.chunk.js
 do
    sed -i 's,'$LOCALHOST','$NEWHOST',g' $jsfile
+   sed -i 's,'$LOCALHOST1','$NEWHOST',g' $jsfile
+
+   sed -i 's,'$LOCALAPIKEY','$NEWTMAPIKEY',g' $jsfile
+
+   sed -i 's,'$LOCALTMHOST','$NEWTMHOST',g' $jsfile
+   sed -i 's,'$LOCALTMHOST1','$NEWTMHOST',g' $jsfile
 done
+
+
+entrypoint_log "$ME: info: Replace API Host url /$NEWHOST"
+
+exit 0
